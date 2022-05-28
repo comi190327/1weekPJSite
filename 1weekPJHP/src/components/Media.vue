571a8aca4f5d8fb432c6ef4fd7c7517fd643533a
comi190327 <comi@actails.com>
2022-05-28 15:56:43 +0900

    Vue.js+TypeScript+Bootstrap5対応

---
+++ 1weekPJHP/src/components/Media.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="Media">
+    <FadeInComponent>
+    <h1>Media</h1>
+    <p>
+        comi's Portfolio<br>
+        Twitter<br>
+        Youtube<br>
+        Github
+    </p>
+    </FadeInComponent>
+</div>
+</template>
+
+<script>
+import FadeInComponent from '@/components/FadeInComponent.vue'
+export default {
+  name: 'Media',
+  components: {
+    FadeInComponent
+  }
+}
+</script>
---
+++ 1weekPJHP/src/components/Schedule.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="schedule">
+    <FadeInComponent>
+    <h1>Streeming schedule</h1>
+    <p>
+        ・5/23(Mon) 15:00～<br>
+        ・5/26(Thu) 23:30～<br>
+        ・5/28(Sat) 15:00～<br>
+        and more!
+    </p>
+    </FadeInComponent>
+</div>
+</template>
+
+<script>
+import FadeInComponent from '@/components/FadeInComponent.vue'
+export default {
+  name: 'What',
+  components: {
+    FadeInComponent
+  }
+}
+</script>
